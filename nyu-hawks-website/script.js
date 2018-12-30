1a37ac0503ef231cdd62b7cc7cdcdc10c1947fc4
Shirley Chin <sc6377@nyu.edu>
2018-12-30 18:37:46 -0500

    added script.js, added new way of updating the side nav news links with js

---
+++ nyu-hawks-website/script.js
@@ -0,0 +1,42 @@
+var nonNewsArticleSideNavList = document.getElementsByClassName('non-news-article-side-nav-list');
+
+var newsArticleSideNavList = document.getElementsByClassName('news-article-side-nav-list');
+
+
+//for updating the non-news article side nav links
+for (var i = 0; i < nonNewsArticleSideNavList.length; i++) {
+    nonNewsArticleSideNavList[i].innerHTML =
+        '<li><a href="news/12-27-18-hawk-sightings-last-three-weeks.html">Reported Hawk Visits Within the Last Three Weeks</a></li>' + 
+        
+        '<li><a href="news/11-28-18-hawk-sightings.html">Reported Hawk Sightings on 11/28/18</a></li>' + 
+        
+        '<li><a href="news/11-20-18-hawk-sightings.html">Adult Hawks Visit the Nest Multiple Times on 11/20/18</a></li>' + 
+        
+        '<li><a href="news/11-13-18-hawk-sighting.html">Reported Hawk Sighting on 11/13/18</a></li>' + 
+        
+        '<li><a href="news/11-12-18-nest-update.html">11/12/18 Nest Update</a></li>' + 
+        
+        '<li><a href="news/10-29-18-hawk-cam-back-online.html">The Hawk Cam is Back Online</a></li>' +
+        
+        '<li><a href="news/10-16-18-hawk-sighting.html">Reported Sightings of Hawks at the Nest on 10/16/18</a></li>';
+};
+
+
+//for updating the news article side nav links
+for (var j = 0; j < newsArticleSideNavList.length; j++) {
+    newsArticleSideNavList[j].innerHTML = 
+        '<li><a href="12-27-18-hawk-sightings-last-three-weeks.html">Reported Hawk Visits Within the Last Three Weeks</a></li>' + 
+        
+        '<li><a href="11-28-18-hawk-sightings.html">Reported Hawk Sightings on 11/28/18</a></li>' + 
+        
+        '<li><a href="11-20-18-hawk-sightings.html">Adult Hawks Visit the Nest Multiple Times on 11/20/18</a></li>' + 
+        
+        '<li><a href="11-13-18-hawk-sighting.html">Reported Hawk Sighting on 11/13/18</a></li>' + 
+        
+        '<li><a href="11-12-18-nest-update.html">11/12/18 Nest Update</a></li>' + 
+        
+        '<li><a href="10-29-18-hawk-cam-back-online.html">The Hawk Cam is Back Online</a></li>' + 
+        
+        '<li><a href="10-16-18-hawk-sighting.html">Reported Sightings of Hawks at the Nest on 10/16/18</a></li>';
+};
+
