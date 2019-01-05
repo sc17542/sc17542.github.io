b843aa9e49851723eda73a0c8795756fff74ff56
Shirley Chin <sc6377@nyu.edu>
2019-01-05 16:04:51 -0500

    added 01-05-19-hawk-sightings.html and adjusted news links

--- nyu-hawks-website/script.js
+++ nyu-hawks-website/script.js
@@ -6,6 +6,8 @@ var newsArticleSideNavList = document.getElementsByClassName('news-article-side-
 //for updating the non-news article side nav links
 for (var i = 0; i < nonNewsArticleSideNavList.length; i++) {
     nonNewsArticleSideNavList[i].innerHTML =
+        '<li><a href="news/2019/01-05-19-hawk-sightings.html">Hawk Visits on 1/5/19</a></li>' + 
+        
         '<li><a href="news/2019/01-04-19-hawk-sightings.html">Hawk Visits in the Past Week</a></li>' + 
         
         '<li><a href="news/2018/12-27-18-hawk-sightings-last-three-weeks.html">Reported Hawk Visits Within the Last Three Weeks</a></li>' + 
@@ -16,15 +18,15 @@ for (var i = 0; i < nonNewsArticleSideNavList.length; i++) {
         
         '<li><a href="news/2018/11-13-18-hawk-sighting.html">Reported Hawk Sighting on 11/13/18</a></li>' + 
         
-        '<li><a href="news/2018/11-12-18-nest-update.html">11/12/18 Nest Update</a></li>' + 
-        
-        '<li><a href="news/2018/10-29-18-hawk-cam-back-online.html">The Hawk Cam is Back Online</a></li>';
+        '<li><a href="news/2018/11-12-18-nest-update.html">11/12/18 Nest Update</a></li>';
 };
 
 
 //for updating the news article side nav links
 for (var j = 0; j < newsArticleSideNavList.length; j++) {
     newsArticleSideNavList[j].innerHTML = 
+        '<li><a href="../2019/01-05-19-hawk-sightings.html">Hawk Visits on 1/5/19</a></li>' + 
+        
         '<li><a href="../2019/01-04-19-hawk-sightings.html">Hawk Visits in the Past Week</a></li>' + 
         
         '<li><a href="../2018/12-27-18-hawk-sightings-last-three-weeks.html">Reported Hawk Visits Within the Last Three Weeks</a></li>' + 
@@ -35,8 +37,6 @@ for (var j = 0; j < newsArticleSideNavList.length; j++) {
         
         '<li><a href="../2018/11-13-18-hawk-sighting.html">Reported Hawk Sighting on 11/13/18</a></li>' + 
         
-        '<li><a href="../2018/11-12-18-nest-update.html">11/12/18 Nest Update</a></li>' + 
-        
-        '<li><a href="../2018/10-29-18-hawk-cam-back-online.html">The Hawk Cam is Back Online</a></li>';
+        '<li><a href="../2018/11-12-18-nest-update.html">11/12/18 Nest Update</a></li>';
 };
 
