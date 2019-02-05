a4887c60f282b0908855fd621777239097f4004e
Shirley Chin <sc6377@nyu.edu>
2019-02-05 01:11:48 -0500

    added 02-05-19-february-hawk-sightings.html and adjusted links with js

--- nyu-hawks-website/script.js
+++ nyu-hawks-website/script.js
@@ -6,6 +6,8 @@ var newsArticleSideNavList = document.getElementsByClassName('news-article-side-
 //for updating the non-news article side nav links
 for (var i = 0; i < nonNewsArticleSideNavList.length; i++) {
     nonNewsArticleSideNavList[i].innerHTML =
+        '<li><a href="news/2019/02-05-19-february-hawk-sightings.html">Hawk Visits in February 2019</a></li>' +
+        
         '<li><a href="news/2019/01-25-19-january-hawk-sightings.html">Hawk Visits in January 2019</a></li>' + 
         
         '<li><a href="news/2019/01-05-19-hawk-sightings.html">Hawk Visits on 1/5/19</a></li>' + 
@@ -16,15 +18,15 @@ for (var i = 0; i < nonNewsArticleSideNavList.length; i++) {
         
         '<li><a href="news/2018/11-28-18-hawk-sightings.html">Reported Hawk Sightings on 11/28/18</a></li>' + 
         
-        '<li><a href="news/2018/11-20-18-hawk-sightings.html">Adult Hawks Visit the Nest Multiple Times on 11/20/18</a></li>' + 
-        
-        '<li><a href="news/2018/11-13-18-hawk-sighting.html">Reported Hawk Sighting on 11/13/18</a></li>';
+        '<li><a href="news/2018/11-20-18-hawk-sightings.html">Adult Hawks Visit the Nest Multiple Times on 11/20/18</a></li>';
 };
 
 
 //for updating the news article side nav links
 for (var j = 0; j < newsArticleSideNavList.length; j++) {
     newsArticleSideNavList[j].innerHTML = 
+        '<li><a href="../2019/02-05-19-february-hawk-sightings.html">Hawk Visits in February 2019</a></li>' +
+        
         '<li><a href="../2019/01-25-19-january-hawk-sightings.html">Hawk Visits in January 2019</a></li>' + 
         
         '<li><a href="../2019/01-05-19-hawk-sightings.html">Hawk Visits on 1/5/19</a></li>' + 
@@ -35,8 +37,6 @@ for (var j = 0; j < newsArticleSideNavList.length; j++) {
         
         '<li><a href="../2018/11-28-18-hawk-sightings.html">Reported Hawk Sightings on 11/28/18</a></li>' + 
         
-        '<li><a href="../2018/11-20-18-hawk-sightings.html">Adult Hawks Visit the Nest Multiple Times on 11/20/18</a></li>' + 
-        
-        '<li><a href="../2018/11-13-18-hawk-sighting.html">Reported Hawk Sighting on 11/13/18</a></li>';
+        '<li><a href="../2018/11-20-18-hawk-sightings.html">Adult Hawks Visit the Nest Multiple Times on 11/20/18</a></li>';
 };
 
