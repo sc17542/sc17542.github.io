eceba634bf8118f342edea7be5929c0d5fd80010
Shirley Chin <sc6377@nyu.edu>
2021-05-09 23:27:20 -0400

    updated website style, added zoom

--- here-and-away/script.js
+++ here-and-away/script.js
@@ -1,10 +1,10 @@
-var pageImg0 = '<img src="images/cover-page.png" alt="Cover page">';
-var pageImg1 = '<img src="images/page0-1.png" alt="Page 1">';
-var pageImg2 = '<img src="images/page2-3.png" alt="Pages 2-3">';
-var pageImg3 = '<img src="images/page4-5.png" alt="Pages 4-5">';
-var pageImg4 = '<img src="images/page6-7.png" alt="Pages 6-7">';
-var pageImg5 = '<img src="images/page8-9.png" alt="Pages 8-9">';
-var pageImg6 = '<img src="images/page10-11.png" alt="Pages 10-11">';
+//var pageImg0 = '<img id="cover-page" src="images/cover-page.png" alt="Cover page">';
+//var pageImg1 = '<img id="page0-1" src="images/page0-1.png" alt="Page 1">';
+//var pageImg2 = '<img id="page2-3" src="images/page2-3.png" alt="Pages 2-3">';
+//var pageImg3 = '<img id="page4-5" src="images/page4-5.png" alt="Pages 4-5">';
+//var pageImg4 = '<img id="page6-7" src="images/page6-7.png" alt="Pages 6-7">';
+//var pageImg5 = '<img id="page8-9" src="images/page8-9.png" alt="Pages 8-9">';
+//var pageImg6 = '<img id="page10-11" src="images/page10-11.png" alt="Pages 10-11">';
 
 var songVid1 = '<video width="100%" autoplay onended="stopSong()"><source src="videos/okay.mp4" type="video/mp4"></video>';
 var songVid2 = '<video width="100%" autoplay onended="stopSong()"><source src="videos/monsters.mp4" type="video/mp4"></video>';
@@ -36,14 +36,59 @@ var pageNumDisplay = document.getElementById("page-num-display");
 
 var currPageNum = 1;
 
+var leftSongBtnDiv = document.getElementById("left-song-btn-div");
+var rightSongBtnDiv = document.getElementById("right-song-btn-div");
+
+var onePageButton = document.getElementById("one-page-button");
+var twoPageButton = document.getElementById("two-page-button");
+var zoomInButton = document.getElementById("zoom-in-button");
+var zoomOutButton = document.getElementById("zoom-out-button");
+
+var coverPage = document.getElementById("cover-page");
+var page01 = document.getElementById("page0-1");
+var page23 = document.getElementById("page2-3");
+var page45 = document.getElementById("page4-5");
+var page67 = document.getElementById("page6-7");
+var page89 = document.getElementById("page8-9");
+var page1011 = document.getElementById("page10-11");
+
+var songVideoDiv = document.getElementById("song-video-div");
+
+var video1 = document.getElementById("video-1");
+var video2 = document.getElementById("video-2");
+var video3 = document.getElementById("video-3");
+var video4 = document.getElementById("video-4");
+var video5 = document.getElementById("video-5");
+
+var currPageSize = 960;
+
+
+coverPage.style.opacity = 1;
+page01.style.opacity = 0;
+page23.style.opacity = 0;
+page45.style.opacity = 0;
+page67.style.opacity = 0;
+page89.style.opacity = 0;
+page1011.style.opacity = 0;
+
 function playSong1() {
-    pageContent.innerHTML = songVid1;
-    buttonDiv.innerHTML = endButton1;
+//    pageContent.innerHTML = songVid1;
+//    songVideoDiv.innerHTML = '<video width="' + currPageSize.toString() + 'px" autoplay onended="stopSong()"><source src="videos/okay.mp4" type="video/mp4"></video>';
+    video1.style.opacity = 1;
+    video1.play();
+    page01.style.opacity = 0;
+//    buttonDiv.innerHTML = endButton1;
+    rightSongBtnDiv.innerHTML = endButton1;
 }
 
 function playSong2() {
-    pageContent.innerHTML = songVid2;
-    buttonDiv.innerHTML = endButton2;
+//    pageContent.innerHTML = songVid2;
+//    songVideoDiv.innerHTML = '<video width="' + currPageSize.toString() + 'px" autoplay onended="stopSong()"><source src="videos/monsters.mp4" type="video/mp4"></video>';
+    video2.style.opacity = 1;
+    video2.play();
+    page23.style.opacity = 0;
+//    buttonDiv.innerHTML = endButton2;
+    rightSongBtnDiv.innerHTML = endButton2;
     setTimeout(function(){
         currPageNum = 4;
         pageNumDisplay.innerHTML = "4/7";
@@ -51,114 +96,243 @@ function playSong2() {
 }
 
 function playSong3() {
-    pageContent.innerHTML = songVid3;
-    buttonDiv.innerHTML = endButton3;
+//    pageContent.innerHTML = songVid3;
+//    songVideoDiv.innerHTML = '<video width="' + currPageSize.toString() + 'px" autoplay onended="stopSong()"><source src="videos/deardestiny.mp4" type="video/mp4"></video>';
+    video3.style.opacity = 1;
+    video3.play();
+    page45.style.opacity = 0;
+//    buttonDiv.innerHTML = endButton3;
+    leftSongBtnDiv.innerHTML = endButton3;
 }
 
 function playSong4() {
-    pageContent.innerHTML = songVid4;
-    buttonDiv.innerHTML = endButton4;
+//    pageContent.innerHTML = songVid4;
+//    songVideoDiv.innerHTML = '<video width="' + currPageSize.toString() + 'px" autoplay onended="stopSong()"><source src="videos/you.mp4" type="video/mp4"></video>';
+    video4.style.opacity = 1;
+    video4.play();
+    page89.style.opacity = 0;
+//    buttonDiv.innerHTML = endButton4;
+    rightSongBtnDiv.innerHTML = endButton4;
     setTimeout(function(){
         currPageNum = 7;
         pageNumDisplay.innerHTML = "7/7";
+        nextButton.disabled = true;
+        nextButton.style.opacity = 0.6;
     }, 39000);
 }
 
 function playSong5() {
-    pageContent.innerHTML = songVid5;
-    buttonDiv.innerHTML = endButton5;
+//    pageContent.innerHTML = songVid5;
+//    songVideoDiv.innerHTML = '<video width="' + currPageSize.toString() + 'px" autoplay onended="stopSong()"><source src="videos/okayreprise.mp4" type="video/mp4"></video>';
+    video5.style.opacity = 1;
+    video5.play();
+    page1011.style.opacity = 0;
+//    buttonDiv.innerHTML = endButton5;
+    rightSongBtnDiv.innerHTML = endButton5;
 }
 
 function stopSong() {
     if (currPageNum == 1) {
-        pageContent.innerHTML = pageImg0;
+//        pageContent.innerHTML = pageImg0;
+        coverPage.style.opacity = 1;
     } else if (currPageNum == 2) {
-        pageContent.innerHTML = pageImg1;
-        buttonDiv.innerHTML = startButton1;
+//        pageContent.innerHTML = pageImg1;
+        video1.style.opacity = 0;
+        video1.pause();
+        video1.currentTime = 0;
+        page01.style.opacity = 1;
+//        buttonDiv.innerHTML = startButton1;
+        rightSongBtnDiv.innerHTML = startButton1;
     } else if (currPageNum == 3) {
-        pageContent.innerHTML = pageImg2;
-        buttonDiv.innerHTML = startButton2;
+//        pageContent.innerHTML = pageImg2;
+        video2.style.opacity = 0;
+        video2.pause();
+        video2.currentTime = 0;
+        page23.style.opacity = 1;
+//        buttonDiv.innerHTML = startButton2;
+        rightSongBtnDiv.innerHTML = startButton2;
     } else if (currPageNum == 4) {
-        pageContent.innerHTML = pageImg3;
-        buttonDiv.innerHTML = startButton3;
+//        pageContent.innerHTML = pageImg3;
+        video3.style.opacity = 0;
+        video3.pause();
+        video3.currentTime = 0;
+        page45.style.opacity = 1;
+//        buttonDiv.innerHTML = startButton3;
+        leftSongBtnDiv.innerHTML = startButton3;
+        rightSongBtnDiv.innerHTML = "";
     } else if (currPageNum == 5) {
-        pageContent.innerHTML = pageImg4;
+//        pageContent.innerHTML = pageImg4;
+        page67.style.opacity = 1;
     } else if (currPageNum == 6) {
-        pageContent.innerHTML = pageImg5;
-        buttonDiv.innerHTML = startButton4;
+//        pageContent.innerHTML = pageImg5;
+        video4.style.opacity = 0;
+        video4.pause();
+        video4.currentTime = 0;
+        page89.style.opacity = 1;
+//        buttonDiv.innerHTML = startButton4;
+        rightSongBtnDiv.innerHTML = startButton4;
     } else if (currPageNum == 7) {
-        pageContent.innerHTML = pageImg6;
-        buttonDiv.innerHTML = startButton5;
+//        pageContent.innerHTML = pageImg6;
+        video5.style.opacity = 0;
+        video5.pause();
+        video5.currentTime = 0;
+        page1011.style.opacity = 1;
+//        buttonDiv.innerHTML = startButton5;
+        rightSongBtnDiv.innerHTML = startButton5;
     }
+//    songVideoDiv.innerHTML = "";
 }
 
 function goToPrevPage() {
     if (currPageNum == 2) {
-        pageContent.innerHTML = pageImg0;
+//        pageContent.innerHTML = pageImg0;
+        coverPage.style.opacity = 1;
+        page01.style.opacity = 0;
         pageNumDisplay.innerHTML = "1/7";
         currPageNum = 1;
-        buttonDiv.innerHTML = "";
+//        buttonDiv.innerHTML = "";
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = "";
+        video1.style.opacity = 0;
+        video1.pause();
+        video1.currentTime = 0;
     } else if (currPageNum == 3) {
-        pageContent.innerHTML = pageImg1;
+//        pageContent.innerHTML = pageImg1;
+        page01.style.opacity = 1;
+        page23.style.opacity = 0;
         pageNumDisplay.innerHTML = "2/7";
         currPageNum = 2;
-        buttonDiv.innerHTML = startButton1;
+//        buttonDiv.innerHTML = startButton1;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton1;
+        video2.style.opacity = 0;
+        video2.pause();
+        video2.currentTime = 0;
     } else if (currPageNum == 4) {
-        pageContent.innerHTML = pageImg2;
+//        pageContent.innerHTML = pageImg2;
+        page23.style.opacity = 1;
+        page45.style.opacity = 0;
         pageNumDisplay.innerHTML = "3/7";
         currPageNum = 3;
-        buttonDiv.innerHTML = startButton2;
+//        buttonDiv.innerHTML = startButton2;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton2;
+        video2.style.opacity = 0;
+        video2.pause();
+        video2.currentTime = 0;
+        video3.style.opacity = 0;
+        video3.pause();
+        video3.currentTime = 0;
     } else if (currPageNum == 5) {
-        pageContent.innerHTML = pageImg3;
+//        pageContent.innerHTML = pageImg3;
+        page45.style.opacity = 1;
+        page67.style.opacity = 0;
         pageNumDisplay.innerHTML = "4/7";
         currPageNum = 4;
-        buttonDiv.innerHTML = startButton3;
+//        buttonDiv.innerHTML = startButton3;
+        leftSongBtnDiv.innerHTML = startButton3;
+        rightSongBtnDiv.innerHTML = "";
     } else if (currPageNum == 6) {
-        pageContent.innerHTML = pageImg4;
+//        pageContent.innerHTML = pageImg4;
+        page67.style.opacity = 1;
+        page89.style.opacity = 0;
         pageNumDisplay.innerHTML = "5/7";
         currPageNum = 5;
-        buttonDiv.innerHTML = "";
+//        buttonDiv.innerHTML = "";
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = "";
+        video4.style.opacity = 0;
+        video4.pause();
+        video4.currentTime = 0;
     } else if (currPageNum == 7) {
-        pageContent.innerHTML = pageImg5
+//        pageContent.innerHTML = pageImg5;
+        page89.style.opacity = 1;
+        page1011.style.opacity = 0;
         pageNumDisplay.innerHTML = "6/7";
         currPageNum = 6;
-        buttonDiv.innerHTML = startButton4;
+//        buttonDiv.innerHTML = startButton4;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton4;
+        video5.style.opacity = 0;
+        video5.pause();
+        video5.currentTime = 0;
     }
+//    songVideoDiv.innerHTML = "";
     updateDisabledButtons();
 }
 
 function goToNextPage() {
     if (currPageNum == 1) {
-        pageContent.innerHTML = pageImg1;
+//        pageContent.innerHTML = pageImg1;
+        coverPage.style.opacity = 0;
+        page01.style.opacity = 1;
         pageNumDisplay.innerHTML = "2/7";
         currPageNum = 2;
-        buttonDiv.innerHTML = startButton1;
+//        buttonDiv.innerHTML = startButton1;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton1;
     } else if (currPageNum == 2) {
-        pageContent.innerHTML = pageImg2;
+//        pageContent.innerHTML = pageImg2;
+        page01.style.opacity = 0;
+        page23.style.opacity = 1;
         pageNumDisplay.innerHTML = "3/7";
         currPageNum = 3;
-        buttonDiv.innerHTML = startButton2;
+//        buttonDiv.innerHTML = startButton2;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton2;
+        video1.style.opacity = 0;
+        video1.pause();
+        video1.currentTime = 0;
     } else if (currPageNum == 3) {
-        pageContent.innerHTML = pageImg3;
+//        pageContent.innerHTML = pageImg3;
+        page23.style.opacity = 0;
+        page45.style.opacity = 1;
         pageNumDisplay.innerHTML = "4/7";
         currPageNum = 4;
-        buttonDiv.innerHTML = startButton3;
+//        buttonDiv.innerHTML = startButton3;
+        leftSongBtnDiv.innerHTML = startButton3;
+        rightSongBtnDiv.innerHTML = "";
+        video2.style.opacity = 0;
+        video2.pause();
+        video2.currentTime = 0;
     } else if (currPageNum == 4) {
-        pageContent.innerHTML = pageImg4;
+//        pageContent.innerHTML = pageImg4;
+        page45.style.opacity = 0;
+        page67.style.opacity = 1;
         pageNumDisplay.innerHTML = "5/7";
         currPageNum = 5;
-        buttonDiv.innerHTML = "";
+//        buttonDiv.innerHTML = "";
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = "";
+        video2.style.opacity = 0;
+        video2.pause();
+        video2.currentTime = 0;
+        video3.style.opacity = 0;
+        video3.pause();
+        video3.currentTime = 0;
     } else if (currPageNum == 5) {
-        pageContent.innerHTML = pageImg5;
+//        pageContent.innerHTML = pageImg5;
+        page67.style.opacity = 0;
+        page89.style.opacity = 1;
         pageNumDisplay.innerHTML = "6/7";
         currPageNum = 6;
-        buttonDiv.innerHTML = startButton4;
+//        buttonDiv.innerHTML = startButton4;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton4;
     } else if (currPageNum == 6) {
-        pageContent.innerHTML = pageImg6;
+//        pageContent.innerHTML = pageImg6;
+        page89.style.opacity = 0;
+        page1011.style.opacity = 1;
         pageNumDisplay.innerHTML = "7/7";
         currPageNum = 7;
-        buttonDiv.innerHTML = startButton5;
+//        buttonDiv.innerHTML = startButton5;
+        leftSongBtnDiv.innerHTML = "";
+        rightSongBtnDiv.innerHTML = startButton5;
+        video4.style.opacity = 0;
+        video4.pause();
+        video4.currentTime = 0;
     }
+//    songVideoDiv.innerHTML = "";
     updateDisabledButtons();
 }
 
@@ -179,4 +353,31 @@ function updateDisabledButtons() {
     }
 }
 
+function zoomIn() {
+    if (currPageSize < 3840) {
+        currPageSize += 480;
+    }
+    updatePageSize();
+}
 
+function zoomOut() {
+    if (currPageSize > 960) {
+        currPageSize -= 480;
+    }
+    updatePageSize();
+}
+
+function updatePageSize() {
+    coverPage.style.width = currPageSize.toString() + "px";
+    page01.style.width = currPageSize.toString() + "px";
+    page23.style.width = currPageSize.toString() + "px";
+    page45.style.width = currPageSize.toString() + "px";
+    page67.style.width = currPageSize.toString() + "px";
+    page89.style.width = currPageSize.toString() + "px";
+    page1011.style.width = currPageSize.toString() + "px";
+    video1.style.width = currPageSize.toString() + "px";
+    video2.style.width = currPageSize.toString() + "px";
+    video3.style.width = currPageSize.toString() + "px";
+    video4.style.width = currPageSize.toString() + "px";
+    video5.style.width = currPageSize.toString() + "px";
+}
